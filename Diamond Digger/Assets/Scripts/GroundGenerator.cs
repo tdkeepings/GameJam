e1a91ebf419b9ddc11d459434e3bb4d6f533a176
Thomas Keepings <tdkeepings@gmail.com>
2015-09-11 22:37:26 +0100

    added light dirt block prefab, added delete all blocks to ground generator

--- Diamond Digger/Assets/Scripts/GroundGenerator.cs
+++ Diamond Digger/Assets/Scripts/GroundGenerator.cs
@@ -12,6 +12,10 @@ public class GroundGenerator : MonoBehaviour
     [ContextMenu("Generate ground")]
     public void GenerateBlocks () 
     {
+        if (generatedBlocks.Count > 0) {
+            DeleteBlocks();
+        }
+
         for (int rows = 0; rows < gridHeight; rows++) 
         {
             for (int columns = 0; columns < gridWidth; columns++)
@@ -19,8 +23,20 @@ public class GroundGenerator : MonoBehaviour
                 GameObject currentBlock = Instantiate(block[Random.Range(0, block.Length)]);
                 generatedBlocks.Add(currentBlock);
                 currentBlock.transform.position = new Vector3(columns * size, rows * size, 0) + transform.position;
+                currentBlock.transform.parent = transform;
             }
         }
+    }
+
+    [ContextMenu("Delete ground")]
+    public void DeleteBlocks() 
+    {
+        foreach (GameObject block in generatedBlocks) 
+        {
+            DestroyImmediate(block);
+        }
+
+        generatedBlocks.Clear();
     }
 
 }
