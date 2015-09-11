26d58afea058fc5003e1bab103f39cef9660b839
Thomas Keepings <tdkeepings@gmail.com>
2015-09-11 22:12:52 +0100

    Added Ground Generator script and dirt assets

---
+++ Diamond Digger/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GroundGenerator : MonoBehaviour 
+{
+    public GameObject[] block;
+    public List<GameObject> generatedBlocks;
+    public int gridWidth = 10;
+    public int gridHeight = 5;
+    public int size = 1;
+
+    [ContextMenu("Generate ground")]
+    public void GenerateBlocks () 
+    {
+        for (int rows = 0; rows < gridHeight; rows++) 
+        {
+            for (int columns = 0; columns < gridWidth; columns++)
+            {
+                GameObject currentBlock = Instantiate(block[Random.Range(0, block.Length)]);
+                generatedBlocks.Add(currentBlock);
+                currentBlock.transform.position = new Vector3(columns * size, rows * size, 0) + transform.position;
+            }
+        }
+    }
+
+}
