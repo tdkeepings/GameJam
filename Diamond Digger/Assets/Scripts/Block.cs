2c98720e50fe5bfa7e0d6bd4cdb3d74f89a8bb23
Karl Jones <karl@unity3d.com>
2015-09-12 12:44:11 +0100

    Removed VS files. Added block script. Added GetNeighbours

--- Diamond Digger/Assets/Scripts/Block.cs
+++ Diamond Digger/Assets/Scripts/Block.cs
@@ -4,8 +4,30 @@ using UnityEngine.Assertions;
 
 public class Block : MonoBehaviour
 {
+	public float health = 100;
+	Animator animator;
+	int healthHash;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		healthHash = Animator.StringToHash("health");
+	}
+
+	public void Dig(float dmg)
+	{
+		health -= dmg;
+		animator.SetFloat(healthHash, health);
+		if(health <= 0)
+			DestroyBlock();
+	}
+
 	void DestroyBlock()
 	{
 		// TOOD: particle/animation of block crumbling
+
+		// Inform our neighbours
+		var controller = GetComponentInParent<GroundGenerator>();
+		
 	}
 }
---
+++ Diamond Digger/Assets/Scripts/BlockNeighbours.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockNeighbours
+{
+	public GameObject left;
+	public GameObject top;
+	public GameObject right;
+	public GameObject bottom;
+}
