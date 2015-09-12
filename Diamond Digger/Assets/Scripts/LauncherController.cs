e52447dea33b7188e8e81826dcc9a3f7034f1efc
Thomas Keepings <tdkeepings@gmail.com>
2015-09-12 18:35:07 +0100

    added rotation and power for launcher

---
+++ Diamond Digger/Assets/Scripts/LauncherController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LauncherController : MonoBehaviour {
+
+    public float Angle;
+    public float Power;
+
+    private SurfaceEffector2D LauncherSE;
+
+	// Use this for initialization
+	void Start () {
+	    LauncherSE = GetComponent<SurfaceEffector2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float angleInput = Input.GetAxis("Horizontal");
+        Angle += angleInput;
+
+        float powerInput = Input.GetAxis("Vertical");
+        Power += powerInput;
+
+        UpdateLauncher();
+	}
+
+    private void UpdateLauncher(){
+        //Power
+        if (Power > 100)
+            Power = 100;
+        else if (Power < 10)
+            Power = 10;
+        else {
+            LauncherSE.speed = Power;
+        }
+
+
+        //Angle
+        if (Angle > 45) {
+            Angle = 45f;
+        } else if (Angle < 10) {
+            Angle = 10f;
+        } else {
+            this.transform.rotation = Quaternion.Euler(0, 0, Angle);
+        }
+    }
+
+}
