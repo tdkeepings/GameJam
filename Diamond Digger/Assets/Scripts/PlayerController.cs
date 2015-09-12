f2339d9f47b5050d3a0d89c627274180f3a11342
Thomas Keepings <tdkeepings@gmail.com>
2015-09-12 18:48:31 +0100

    added space to start / drop player

--- Diamond Digger/Assets/Scripts/PlayerController.cs
+++ Diamond Digger/Assets/Scripts/PlayerController.cs
@@ -3,33 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float maxSpeed = 10f;
-    bool facingRight = true;
-
-    Animator anim;
-
-	// Use this for initialization
+    private bool playerIsLocked = true;
+    private Rigidbody2D player;
+    private float initialGravity;
 	void Start () {
-        anim = GetComponent<Animator>();
+        player = GetComponent<Rigidbody2D>();
+        initialGravity = player.gravityScale;
+        player.gravityScale = 0f;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float move = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown("space")) {
+            playerIsLocked = false;
+        }
 
-        anim.SetFloat("Speed", Mathf.Abs(move));
-
-        Rigidbody2D rigidBody2D = GetComponent<Rigidbody2D>();
-        rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
-
-        if (move > 0 && !facingRight) Flip();
-        else if (move < 0 && facingRight) Flip();
+        if (!playerIsLocked) {
+            player.gravityScale = initialGravity;
+        }    
 	}
 
-    private void Flip() {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
 }
