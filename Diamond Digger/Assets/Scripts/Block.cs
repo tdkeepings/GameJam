c8ca162650403efaef8a9c3c287a721ddd0ebc1b
Karl Jones <karl@unity3d.com>
2015-09-12 11:46:45 +0100

    Added box colliders. Change names of generated objects. Added random seed so we can generate the same levels for testing etc.

---
+++ Diamond Digger/Assets/Scripts/Block.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class Block : MonoBehaviour
+{
+	void DestroyBlock()
+	{
+		// TOOD: particle/animation of block crumbling
+	}
+}
--- Diamond Digger/Assets/Scripts/GroundGenerator.cs
+++ Diamond Digger/Assets/Scripts/GroundGenerator.cs
@@ -3,40 +3,42 @@ using System.Collections.Generic;
 
 public class GroundGenerator : MonoBehaviour 
 {
-    public GameObject[] block;
-    public List<GameObject> generatedBlocks;
-    public int gridWidth = 10;
-    public int gridHeight = 5;
-    public int size = 1;
+	public GameObject[] block;
+	public List<GameObject> generatedBlocks;
+	public int gridWidth = 10;
+	public int gridHeight = 5;
+	public int size = 1;
+	public int randomSeed = 123;
 
-    [ContextMenu("Generate ground")]
-    public void GenerateBlocks () 
-    {
-        if (generatedBlocks.Count > 0) {
-            DeleteBlocks();
-        }
+	[ContextMenu("Generate ground")]
+	public void GenerateBlocks () 
+	{
+		if (generatedBlocks.Count > 0) {
+			DeleteBlocks();
+		}
 
-        for (int rows = 0; rows < gridHeight; rows++) 
-        {
-            for (int columns = 0; columns < gridWidth; columns++)
-            {
-                GameObject currentBlock = Instantiate(block[Random.Range(0, block.Length)]);
-                generatedBlocks.Add(currentBlock);
-                currentBlock.transform.position = new Vector3(columns * size, rows * size, 0) + transform.position;
-                currentBlock.transform.parent = transform;
-            }
-        }
-    }
+		Random.seed = randomSeed;
+		for (int vertical = 0; vertical < gridHeight; vertical++) 
+		{
+			for (int horizontal = 0; horizontal < gridWidth; horizontal++)
+			{
+				GameObject currentBlock = Instantiate(block[Random.Range(0, block.Length)]);
+				generatedBlocks.Add(currentBlock);
+				currentBlock.name = horizontal + "," + vertical;
+				currentBlock.transform.position = new Vector3(horizontal * size, vertical * size, 0) + transform.position;
+				currentBlock.transform.parent = transform;
+			}
+		}
+	}
 
-    [ContextMenu("Delete ground")]
-    public void DeleteBlocks() 
-    {
-        foreach (GameObject block in generatedBlocks) 
-        {
-            DestroyImmediate(block);
-        }
+	[ContextMenu("Delete ground")]
+	public void DeleteBlocks() 
+	{
+		foreach (GameObject block in generatedBlocks) 
+		{
+			DestroyImmediate(block);
+		}
 
-        generatedBlocks.Clear();
-    }
-
+		generatedBlocks.Clear();
+	}
 }
